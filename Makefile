497c1916dffeb537f85339784abf5429e2125588
tiennguyen047 <tiennguyen047@gmail.com>
2023-02-02 21:24:33 +0700

    KT2: create Makefile for build
    - Create Makefile
    - Create bash scrip build project
    - Create script bash for run main.jar

---
+++ Makefile
@@ -0,0 +1,14 @@
+# This Makefile will run common task for kotllin
+# make : build main.kt 
+
+SCRIPT_DIR = build
+
+# Build main.kt, always the FIRST target in this Makefile
+build: info
+	@bash $(SCRIPT_DIR)/build.sh
+
+info:
+	echo $("make file for build project")
+
+run:
+	@bash $(SCRIPT_DIR)/run.sh
---
+++ build/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_REPO="$(git rev-parse --show-toplevel)"
+
+
+function f_print_Params() {
+    
+    echo "path of build.bash is:    ${SCRIPT_DIR}"
+    echo "git dir:                  ${PROJECT_REPO}"
+    # echo "NQT"
+}
+
+function f_printUsage() {
+    echo Usage:
+    echo "-b build with tag. Default:  ${SCRIPT_DIR} "
+    echo "-h help "
+}
+
+function f_build_main() {
+    echo "Start build main.jar"
+    printf "Run command line build kotlin => main.jar \n"
+    kotlinc $PROJECT_REPO/project/src/main.kt -include-runtime -d $SCRIPT_DIR/main.jar
+    echo "Build sucessfully $SCRIPT_DIR/main.jar"
+}
+
+###################################################################
+#Main function
+#
+###################################################################
+
+f_print_Params
+f_printUsage
+f_build_main
